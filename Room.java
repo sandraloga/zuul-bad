d9becf2f8368291567ea28b50d76336a9f7d214e
Sandra <72sandraloga@gmail.com>
2016-04-11 13:02:34 +0200

    Modificado el proyecto para que el jugador pueda moverse también en dirección noroeste

--- Room.java
+++ Room.java
@@ -20,6 +20,7 @@ public class Room
     private Room eastExit;
     private Room westExit;
     private Room southEastExit;
+    private Room northWestExit;
 
     /**
      * Create a room described "description". Initially, it has
@@ -40,7 +41,7 @@ public class Room
      * @param south The south exit.
      * @param west The west exit.
      */
-    public void setExits(Room north, Room east, Room south, Room west, Room southEast) 
+    public void setExits(Room north, Room east, Room south, Room west, Room southEast , Room northWest) 
     {
         if(north != null)
             northExit = north;
@@ -52,6 +53,8 @@ public class Room
             westExit = west;
         if (southEast != null)
             southEastExit = southEast;
+        if (northWest != null)
+            northWestExit = northWest;
 
     }
 
@@ -81,6 +84,10 @@ public class Room
         if (direcion.equals("southEast")){
             roomDeVuelta = southEastExit;
         }
+        if (direcion.equals("northWestnorthWest")){
+             roomDeVuelta = northWestExit;
+        }
+       
         return roomDeVuelta;
 
     }
@@ -108,6 +115,9 @@ public class Room
         }
         if (southEastExit != null){
             salida += "southEast ";
+        }
+        if (northWestExit != null){
+            salida += "northWest ";
         }
         return salida;
     
