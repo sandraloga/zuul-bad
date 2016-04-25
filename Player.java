cc8e845cc535d0050310e38b4f0351ad70ac38b7
Sandra <72sandraloga@gmail.com>
2016-04-25 10:45:26 +0200

    Añadidos metodos para que el jugador pueda coger items y añadirle peso

--- Player.java
+++ Player.java
@@ -9,6 +9,8 @@ public class Player
 {
     // instance variables - replace the example below with your own
     private ArrayList<Item> itemsPlayer;
+    private float pesoTotal;
+    private static final float LIMITE_PESO= 50;
 
     /**
      * Constructor for objects of class Player
@@ -17,6 +19,7 @@ public class Player
     {
         // initialise instance variables
         itemsPlayer = new ArrayList<Item>();
+        pesoTotal = 0;
     }
 
     /**
@@ -26,6 +29,22 @@ public class Player
     public void addItem(Item item)
     {
         
-        itemsPlayer.add(item); 
+        itemsPlayer.add(item);
+        
+    }
+    /**
+     * devuelve el peso del jugador
+     */
+    public float getPesoTotal()
+    {
+        return pesoTotal;
+    }
+    
+    /**
+     * suma del peso al peso tota
+     */
+    public void setPesoTotal( float peso)
+    {
+        pesoTotal += peso;
     }
 }
