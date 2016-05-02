09b063b5ac2712a790aa2a52dfb370b92ad312e1
Sandra <72sandraloga@gmail.com>
2016-05-02 18:08:10 +0200

    Los comandos ahora estan en la clse Option

--- Option.java
+++ Option.java
@@ -5,8 +5,21 @@
  * @author (your name here)
  * @version (version number or date here)
  */
-public enum Option
-{
-    GO, QUIT, HELP, LOOK, EAT, BACK, TAKE, DROP, ITEMS, UNKNOWN;
+public enum Option{
+
+  
+    GO("ir") , QUIT("salir"), HELP("ayuda"), LOOK("mirar"), EAT("comer"), BACK("volver"), 
+        TAKE("coger"), DROP("dejar"), ITEMS("items"), UNKNOWN(" ");
+    private String comando;
+    
+    private Option (String comando){
+        this.comando = comando;
+    }
+    public String getComando(){
+        return comando;
+    }
+        
+    
+    
     
 }
